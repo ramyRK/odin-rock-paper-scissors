28ad0ddf795cf5253740a468f937e90e960829a0
ramy <kheliliramy0@gmail.com>
2025-11-24 15:34:59 +0100

    palyRound function

--- game.js
+++ game.js
@@ -2,17 +2,45 @@ function getComputerChoice(){
     let choice =Math.floor((Math.random()*3)+1)
     switch(choice){
         case 1:
-            return 'Rock'
+            return 'rock';
         case 2:
-            return 'Paper'
+            return 'paper';
         case 3:
-            return 'Scissors'        
+            return 'scissors';        
     }
 }
 
 function getHumanChoice(){
-    choice=prompt('enter your choice:')
-    return choice
+    choice=prompt('enter your choice:');
+    return choice;
 }
-console.log(getHumanChoice())
-console.log(getComputerChoice())
+
+
+
+let humanScore = 0;
+let computerScore = 0;
+
+
+    function playRound(humanChoice,computerChoice){
+        const human = humanChoice.toLowerCase();
+
+        if (human === computerChoice){
+            console.log('its a tie');
+            
+        }else{
+
+        if (
+            (human==='rock'&&computerChoice==='scissors')||
+            (human==='scissors'&&computerChoice==='paper')||
+            (human==='paper'&&computerChoice==='rock')
+        ){
+            humanScore++;
+            console.log(`You win!! ${human} beats ${computerChoice}`);
+        }else{
+            computerScore++;
+            console.log(`You lose!! ${computerChoice} beats ${human}`);
+        }
+    }
+    }
+
+    playRound(getHumanChoice(),getComputerChoice());    
